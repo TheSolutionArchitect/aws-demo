34fb81123175ab879d20f368b0e0590a1714a81b
AWS Tech Guide <62712515+awstechguide@users.noreply.github.com>
2020-08-27 10:26:22 -0400

    adding springboot-s3 project

--- springboot-dynamodb/src/main/java/com/awstechguide/aws/springbootdynamodb/SpringbootDynamodbApplication.java
+++ springboot-dynamodb/src/main/java/com/awstechguide/aws/springbootdynamodb/SpringbootDynamodbApplication.java
@@ -1,13 +1,31 @@
 package com.awstechguide.aws.springbootdynamodb;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
 
 @SpringBootApplication
 public class SpringbootDynamodbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDynamodbApplication.class, args);
+		
+		BasicAWSCredentials credentials = new BasicAWSCredentials("AKIAUOWLFFMUHQFGEQO3", "WYw19X7ua55O4Fcq0I+2z4ZCeSEMNEYsnKC99/jJ");
+		AmazonS3 s3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
+		
+		//final AmazonS3 s3 = AmazonS3ClientBuilder.standard().build();
+		List<Bucket> buckets = s3.listBuckets();
+		buckets.stream().forEach(bucket -> {
+			System.out.println("bucket name: " + bucket.getName() + " Create date: " + bucket.getCreationDate()
+					+ " get Owner: " + bucket.getOwner());
+		});
 	}
 
 }
