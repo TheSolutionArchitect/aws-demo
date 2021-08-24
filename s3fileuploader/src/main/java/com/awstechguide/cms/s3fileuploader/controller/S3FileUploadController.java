53bdf58adad271e1bab5e4f68c49870177d35a88
AWS Tech Guide <62712515+awstechguide@users.noreply.github.com>
2021-08-24 10:23:05 -0400

    updated controller and service impl

--- s3fileuploader/src/main/java/com/awstechguide/cms/s3fileuploader/controller/S3FileUploadController.java
+++ s3fileuploader/src/main/java/com/awstechguide/cms/s3fileuploader/controller/S3FileUploadController.java
@@ -1,5 +1,43 @@
 package com.awstechguide.cms.s3fileuploader.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.awstechguide.cms.s3fileuploader.service.S3FileUploadService;
+
+@RestController
+@RequestMapping("/file")
 public class S3FileUploadController {
 
+	 @Autowired
+	    private S3FileUploadService service;
+
+	    @PostMapping("/upload")
+	    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
+	        return new ResponseEntity<>(service.uploadFile(file), HttpStatus.OK);
+	    }
+
+	    @GetMapping("/download/{fileName}")
+	    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
+	        byte[] data = service.downloadFile(fileName);
+	        ByteArrayResource resource = new ByteArrayResource(data);
+	        return ResponseEntity
+	                .ok()
+	                .contentLength(data.length)
+	                .header("Content-type", "application/octet-stream")
+	                .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
+	                .body(resource);
+	    }
+
+	    @DeleteMapping("/delete/{fileName}")
+	    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
+	        return new ResponseEntity<>(service.deleteFile(fileName), HttpStatus.OK);
+	    }
+	
+	
 }
--- s3fileuploader/src/main/java/com/awstechguide/cms/s3fileuploader/service/S3FileUploadService.java
+++ s3fileuploader/src/main/java/com/awstechguide/cms/s3fileuploader/service/S3FileUploadService.java
@@ -1,5 +1,13 @@
 package com.awstechguide.cms.s3fileuploader.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public interface S3FileUploadService {
+
+	String uploadFile(MultipartFile file);
+
+	byte[] downloadFile(String fileName);
+
+	String deleteFile(String fileName);
 
 }
